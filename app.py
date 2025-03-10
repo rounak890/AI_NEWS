12b4f8e3e24f50494ba109f1385783e85b7e3c65
rounak890 <gerarounak890@gmail.com>
2025-03-10 10:06:28 +0530

    local to online

--- app.py
+++ app.py
@@ -63,8 +63,9 @@ app.add_middleware(
 
 
 # Database Setup
-DATABASE_URL = "sqlite:///./users.db"
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+# DATABASE_URL = "sqlite:///./users.db"
+DATABASE_URL = os.getenv("DATABASE_URL")
+engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
